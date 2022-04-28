6db57ce1db2fe08ec59ead1e25da8a72c033e9f0
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-04-28 20:09:00 -0300

    Aula01

---
+++ works/aula1/ex04.js
@@ -0,0 +1,57 @@
+import * as THREE from  'three';
+import Stats from       '../../build/jsm/libs/stats.module.js';
+import {TrackballControls} from '../../build/jsm/controls/TrackballControls.js';
+import {initRenderer, 
+        initCamera,
+        initDefaultBasicLight,
+        InfoBox,
+        onWindowResize,
+        createGroundPlaneXZ} from "../../libs/util/util.js";
+
+var scene = new THREE.Scene();    // Create main scene
+var renderer = initRenderer();    // View function in util/utils
+var camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
+initDefaultBasicLight(scene);
+
+// Enable mouse rotation, pan, zoom etc.
+var trackballControls = new TrackballControls( camera, renderer.domElement );
+
+// Show axes (parameter is size of each axis)
+var axesHelper = new THREE.AxesHelper( 12 );
+scene.add( axesHelper );
+
+// create the ground plane
+let plane = createGroundPlaneXZ(20, 20)
+scene.add(plane);
+
+for(let i=0; i<9; i++) {
+   // create a cube 1
+   var cubeGeometry1 = new THREE.BoxGeometry(2, 2, 2);
+   var cubeMaterial1 = new THREE.MeshNormalMaterial();
+   var cube1 = new THREE.Mesh(cubeGeometry1, cubeMaterial1);
+   // position the cube
+   cube1.position.set(0.0+(4*i), 2.0+(2*i), 0.0+(2*i));
+   // add the cube to the scene
+   scene.add(cube1);
+}
+
+// Use this to show information onscreen
+var controls = new InfoBox();
+  controls.add("Basic Scene");
+  controls.addParagraph();
+  controls.add("Use mouse to interact:");
+  controls.add("* Left button to rotate");
+  controls.add("* Right button to translate (pan)");
+  controls.add("* Scroll to zoom in/out.");
+  controls.show();
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+render();
+function render()
+{
+  trackballControls.update(); // Enable mouse movements
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
+}
